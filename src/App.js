5e00b68da18fe2f90d7b9dd40939a8f26b37f99b
Matt <mddever@gmail.com>
2019-07-01 16:44:38 -0600

    added maping of users

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UserTable from './tables/UserTable'
+import AddUserForm from './forms/AddUserForm'
 
 
 const App = () => {
+
+  const usersData= [
+    { id: 1, name: "name 1", username: 'user1' },
+    { id: 2, name: "name 2", username: 'user2' },
+    { id: 3, name: "name 3", username: 'user3' },
+  ]
+
+  const [users, setUsers] = useState(usersData)
+
+  const addUser = user => {
+    user.id = users.length + 1
+    setUsers([...useState, user])
+  }
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
       <div className="flex-row">
         <div className="flex-large">
           <h2>Add User</h2>
+          <AddUserForm addUser={addUser}/>
         </div>
         <div className="flex-large">
           <h2> View Users</h2>
-          <UserTable />
+          <UserTable users={users}/>
         </div>
       </div>
     </div>
